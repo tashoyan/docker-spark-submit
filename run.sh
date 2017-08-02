44c5c200c1a48f6dc5813686b5065d7de49871a9
Arseniy Tashoyan <tashoyan@gmail.com>
2017-08-02 23:08:13 +0300

    More info output

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ test -z "$SCM_URL" && ( echo "SCM_URL is not set; exiting" ; exit 1 )
 test -z "$SPARK_MASTER" && ( echo "SPARK_MASTER is not set; exiting" ; exit 1 )
 test -z "$MAIN_CLASS" && ( echo "MAIN_CLASS is not set; exiting" ; exit 1 )
 
-echo "Cloning the repository"
+echo "Cloning the repository: $SCM_URL"
 git clone "$SCM_URL"
 project_git="${SCM_URL##*/}"
 project_dir="${project_git%.git}"
@@ -17,8 +17,7 @@ then
   cd -
 fi
 
-# Hardcoded URL and local path
-# Assembly, fallback to package
+# TODO Assembly, fallback to package
 echo "Building the jar"
 if test -z "$PROJECT_SUBDIR"
 then
@@ -26,6 +25,7 @@ then
 else
   cd "$project_dir/$PROJECT_SUBDIR"
 fi
+echo "Building at: $(pwd)"
 if test -z "SKIP_TESTS"
 then
   sbt clean assembly
@@ -33,7 +33,7 @@ else
   sbt "set test in assembly := {}" clean assembly
 fi
 
-echo "Running the application"
+echo "Running the application: $MAIN_CLASS at Spark master: $SPARK_MASTER"
 jarfile="$(find target/ -type f -name *-assembly*.jar)"
 spark-submit --master "$SPARK_MASTER" --class "$MAIN_CLASS" $jarfile
 
