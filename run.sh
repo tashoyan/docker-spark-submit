c7414bdfea93d636cc58ce58df91ed4cb84dbfc1
Arseniy Tashoyan <tashoyan@gmail.com>
2017-08-13 11:47:33 +0300

    Handle exceptional case: cannot determine IP address of the container

--- run.sh
+++ run.sh
@@ -38,6 +38,11 @@ else
 fi
 
 ip_addr="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]+\.){3}[0-9]+' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+if test -z "$ip_addr"
+then
+  echo "Cannot determine the container IP address."
+  exit 1
+fi
 
 #TODO How to pass app args?
 jarfile="$(find target/ -type f -name *-assembly*.jar)"
