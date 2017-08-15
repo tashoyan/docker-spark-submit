fdad8e54ede85ee6465290e04e606314106440d0
Arseniy Tashoyan <tashoyan@gmail.com>
2017-08-16 00:51:33 +0300

    Remove hardcoded addresses

--- examples/observatory.sh
+++ examples/observatory.sh
@@ -3,6 +3,13 @@
 set -o nounset
 set -o errexit
 
+ip_addr="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]+\.){3}[0-9]+' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+if test -z "$ip_addr"
+then
+  echo "Cannot determine the container IP address."
+  exit 1
+fi
+
 docker run \
   -ti \
   --rm \
@@ -12,8 +19,8 @@ docker run \
   -e SCM_URL="https://github.com/tashoyan/sc.git" \
   -e SCM_BRANCH="spark" \
   -e PROJECT_SUBDIR="05-functional-programming-in-scala-capstone/observatory" \
-  -e SPARK_MASTER="spark://16.17.90.70:7077" \
+  -e SPARK_MASTER="spark://localhost:7077" \
   -e MAIN_CLASS="image.Generator0S" \
-  -e SPARK_DRIVER_HOST="16.17.90.70" \
+  -e SPARK_DRIVER_HOST="$ip_addr" \
   -e https_proxy="http://web-proxy.gre.hpecorp.net:8080" \
   tashoyan/docker-spark-submit:spark-2.2.0
--- examples/timeusage.sh
+++ examples/timeusage.sh
@@ -3,6 +3,13 @@
 set -o nounset
 set -o errexit
 
+ip_addr="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]+\.){3}[0-9]+' | grep -Eo '([0-9]+\.){3}[0-9]+' | grep -v '127.0.0.1')"
+if test -z "$ip_addr"
+then
+  echo "Cannot determine the container IP address."
+  exit 1
+fi
+
 docker run \
   -ti \
   --rm \
@@ -13,11 +20,11 @@ docker run \
   -e SCM_BRANCH="spark" \
   -e PROJECT_SUBDIR="04-big-data-analysis-with-scala-and-spark/04-timeusage/timeusage" \
   -e BUILD_COMMAND="sbt clean package" \
-  -e SPARK_MASTER="spark://16.17.90.70:7077" \
+  -e SPARK_MASTER="spark://localhost:7077" \
   -e SPARK_CONF="--conf spark.executor.memory=2g --conf spark.driver.cores=2" \
   -e JAR_FILE="target/scala-2.11/bigdata-timeusage_2.11-0.1-SNAPSHOT.jar" \
   -e MAIN_CLASS="timeusage.TimeUsage" \
   -e APP_ARGS="aaa bbb" \
-  -e SPARK_DRIVER_HOST="16.17.90.70" \
+  -e SPARK_DRIVER_HOST="$ip_addr" \
   -e https_proxy="http://web-proxy.gre.hpecorp.net:8080" \
   tashoyan/docker-spark-submit:spark-2.2.0
